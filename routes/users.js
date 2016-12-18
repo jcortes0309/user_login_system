40c171146878533f32ddd6c3dd0fe0b1cd86babc
Juan A Cortes <juan.cortes@gmail.com>
2016-12-18 01:05:56 -0500

    Implement logout and access control

--- routes/users.js
+++ routes/users.js
@@ -104,7 +104,12 @@ router.post('/register', upload.single("profileimage"), function(req, res, next)
     res.location("/");
     res.redirect("/");
   }
+});
 
+router.get("/logout", function(req, res) {
+  req.logout();
+  req.flash("success", "You are now logged out");
+  res.redirect("/users/login");
 });
 
 
